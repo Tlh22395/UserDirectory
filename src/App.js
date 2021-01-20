b003c3b41d087e2ec84e69f8dcfa5217e6d4f5b2
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-01-20 11:12:06 -0500

    added to components, added search top search and header, also added to app.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import TopSearch from "./components/TopSearch";
+
+function App () {
+    return (
+        <div>
+            <Header />
+            <TopSearch />
+
+            
+        </div>
+    )
+
+}
+
+export default App;
